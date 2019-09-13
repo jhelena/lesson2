07745275c02dbf35da44a5e1aeab0db6583ed759
Elena Yakunicheva <jhelena@yandex.ru>
2019-09-13 21:12:38 +0300

    ver3

--- while1.py
+++
@@ -1,9 +0,0 @@
-def ask_user():
-    while True:
-        user_say = input('Как дела?: ')
-        if user_say.lower() == "хорошо":
-            print('Ну и отлично!')
-            break
-
-ask_user()
-   
